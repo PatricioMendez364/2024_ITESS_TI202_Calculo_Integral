d2a20a45dcb326a1551592953406bc2389643226
Pato <patoloco22.34@gmail.com>
2024-05-08 13:32:08 -0600

    Se creo una funcion, se integro y se derivo. 8 de marzo de 2024

--- Derivadas_e_Integrales.m
+++ Derivadas_e_Integrales.m
@@ -1,2 +1,32 @@
 %% ITESS
 %% 6 de mayo
+%%Patricio Arturo Mendez Gonzalez
+
+pkg load  symbolic
+syms x
+
+%%Definir una funcion
+
+disp('Una funcion')
+
+fx = x * sin(x)
+
+%%Derivar
+
+disp('Derivada')
+
+dfx = diff(fx,x)
+
+%%Integrar
+
+disp('Integral indefinida')
+
+ifx = int(fx, x)
+
+%%Intergral definida
+
+disp('Integral definida')
+
+ifx = double( int(fx, x, 0, 1))
+
+
